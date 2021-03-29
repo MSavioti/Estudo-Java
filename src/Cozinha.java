c874d88c2195cd7c35f6793885d546c92202541e
Angelo <matheussavioti@gmail.com>
2021-03-29 16:57:32 -0300

    lista de Ingrediente adicionada à classe Cozinha

--- src/Cozinha.java
+++ src/Cozinha.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+
 public class Cozinha {
     private int horaAbertura;
     private int horaFechamento;
     private String pratoPrincipal;
+    private ArrayList<Ingrediente> ingredientes;
 
     public Cozinha(int horaAbertura, int horaFechamento, String pratoPrincipal) {
         this.horaAbertura = horaAbertura;
         this.horaFechamento = horaFechamento;
         this.pratoPrincipal = pratoPrincipal;
+        ingredientes = new ArrayList<>();
     }
 }
