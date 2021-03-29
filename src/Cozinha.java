57bb061175cea99bc4718031d10582c76a616450
Angelo <matheussavioti@gmail.com>
2021-03-29 16:42:48 -0300

    classe cozinha criada

---
+++ src/Cozinha.java
@@ -0,0 +1,11 @@
+public class Cozinha {
+    private int horaAbertura;
+    private int horaFechamento;
+    private String pratoPrincipal;
+
+    public Cozinha(int horaAbertura, int horaFechamento, String pratoPrincipal) {
+        this.horaAbertura = horaAbertura;
+        this.horaFechamento = horaFechamento;
+        this.pratoPrincipal = pratoPrincipal;
+    }
+}
