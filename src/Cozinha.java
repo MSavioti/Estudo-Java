378d61975c715eaf20365e6848d518e8c6d89635
Angelo <matheussavioti@gmail.com>
2021-03-29 17:05:55 -0300

    adicionada lista de Funcionario na classe Cozinha

--- src/Cozinha.java
+++ src/Cozinha.java
@@ -5,11 +5,13 @@ public class Cozinha {
     private int horaFechamento;
     private String pratoPrincipal;
     private ArrayList<Ingrediente> ingredientes;
+    private ArrayList<Funcionario> funcionarios;
 
     public Cozinha(int horaAbertura, int horaFechamento, String pratoPrincipal) {
         this.horaAbertura = horaAbertura;
         this.horaFechamento = horaFechamento;
         this.pratoPrincipal = pratoPrincipal;
         ingredientes = new ArrayList<>();
+        funcionarios = new ArrayList<>();
     }
 }
